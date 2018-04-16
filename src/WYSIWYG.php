86afefb56adb38d4ddb262e2c1513d7fcc3bdaf7
Gemma Black <gblackuk@googlemail.com>
2018-04-16 16:49:29 +0100

    WYSIWYG character counter added

--- src/WYSIWYG.php
+++ src/WYSIWYG.php
@@ -2,12 +2,20 @@
 
 namespace WCom\WYSIWYG;
 
+/**
+ * WYSIWYG class
+ * @author Gemma Black <gblackuk@gmail.com>
+ */
 class WYSIWYG
 {
+    /**
+     * Registers the wysiwyg script with the enqueued admin scripts
+     * @return void
+     */
     public static function registerCharacterCount()
     {
         add_action('admin_enqueue_scripts', function() {
-            wp_enqueue_script('wysiwyg_char_counter', plugins_url(__DIR__) . '/../assets/wysiwyg-character-counter.js');
+            wp_enqueue_script('wysiwyg_char_counter', plugins_url('/../assets/wysiwyg-character-counter.js', __FILE__));
         });
     }
 }
